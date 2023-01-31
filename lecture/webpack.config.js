e2cb42222261ab29ead6f99c26869026914eed46
chuya <choo_bk@naver.com>
2023-01-31 20:17:44 +0900

    Study: webpack

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,49 +1,19 @@
-const path = require('path');
-const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-  name: 'wordrelay',
-  mode: 'development', // 실서비스: production
-  devtool: 'eval', // 실서비스: hidden-source-map
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-
-  entry: {
-    app: ['./client'],
-  }, // 입력
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          ['@babel/preset-env', {
-            targets: {
-              browsers: ['> 1% in KR'], // browserslist
-            },
-            debug: true,
-          }],
-          '@babel/preset-react',
-        ],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          'react-refresh/babel',
-        ],
-      },
-    }],
-  },
-  plugins: [
-    new RefreshWebpackPlugin()
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.js',
-    publicPath: '/dist/',
-  },
-  devServer: {
-    devMiddleware: { publicPath: '/dist' },
-    static: { directory: path.resolve(__dirname) },
-    hot: true
-  },
+    name: "wordrelay-setting", //webpack설정 이름
+    mode: "development", // 실서비스:production
+    devtool: "eval",
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    entry: {
+        //입력
+        app: ["./client.jsx", "./WordRelay.jsx"],
+    },
+    output: {
+        //출력
+        path: path.join(__dirname, "dist"), // __dirname : 현재 폴더 경로
+        filename: "app.js",
+    },
 };

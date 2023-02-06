3c14f74ceb58527a953080180d678e3b8f5daff2
chu <71359092+choo-bo-kyoung@users.noreply.github.com>
2023-02-06 22:57:38 +0900

    error

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,19 +1,32 @@
 const path = require("path");
 
 module.exports = {
-    name: "wordrelay-setting", //webpack설정 이름
-    mode: "development", // 실서비스:production
-    devtool: "eval",
-    resolve: {
-        extensions: [".js", ".jsx"],
-    },
-    entry: {
-        //입력
-        app: ["./client.jsx", "./WordRelay.jsx"],
-    },
-    output: {
-        //출력
-        path: path.join(__dirname, "dist"), // __dirname : 현재 폴더 경로
-        filename: "app.js",
-    },
+  name: "wordrelay-setting", //webpack설정 이름
+  mode: "development", // 실서비스:production
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: {
+    //입력
+    app: ["./client.jsx", "./WordRelay.jsx"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        // loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+
+  output: {
+    //출력
+    path: path.join(__dirname, "dist"), // __dirname : 현재 폴더 경로
+    filename: "app.js",
+  },
 };
